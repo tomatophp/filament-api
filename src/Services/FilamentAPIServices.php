40a63b84afae25c4885fef52af3891c063adf9a3
toonvd <toon_vd@live.com>
2024-10-02 23:45:34 +0200

    support relations

--- src/Services/FilamentAPIServices.php
+++ src/Services/FilamentAPIServices.php
@@ -5,41 +5,29 @@ namespace TomatoPHP\FilamentApi\Services;
 use Closure;
 use Filament\Forms\Form;
 use Filament\Pages\Page;
-use Filament\Resources\Pages\CreateRecord;
-use Filament\Resources\Pages\EditRecord;
-use Filament\Resources\Pages\ListRecords;
-use Filament\Resources\Pages\ManageRecords;
-use Filament\Resources\Pages\ViewRecord;
-use Filament\Resources\Resource;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\Rules\Unique;
 use TomatoPHP\FilamentApi\Helpers\APIResponse;
 
 class FilamentAPIServices
 {
-    private ?Page $page=null;
-    private ?Table $table=null;
-    private ?Form $form=null;
-    private ?array $routes=[];
+    private ?array $routes = [];
 
     public function register(
         Page $page,
         Closure $form,
-        ?Closure $table=null,
-        ?string $type=null,
-        ?string $resource=null,
-        ?array $middleware=null,
-        ?string $slug=null
-    ): array
-    {
+        ?Closure $table = null,
+        ?string $type = null,
+        ?string $resource = null,
+        ?array $middleware = null,
+        ?string $slug = null
+    ): array {
         return $this->generate(
             $page,
             $form(new Form($page)),
-            $table? $table(new Table($page)) : null,
+            $table ? $table(new Table($page)) : null,
             $type,
             $resource,
             $middleware,
@@ -50,36 +38,35 @@ class FilamentAPIServices
     public function generate(
         Page $page,
         Form $form,
-        ?Table $table=null,
-        ?string $type=null,
-        ?string $resource=null,
-        ?array $middleware=null,
-        ?string $slug=null
-    ): array
-    {
+        ?Table $table = null,
+        ?string $type = null,
+        ?string $resource = null,
+        ?array $middleware = null,
+        ?string $slug = null
+    ): array {
         $routes = [];
-        $query = $page::getResource()::getEloquentQuery();
+        $query = app($page::getResource()::getModel());
         $slug  = $slug ?? $page::getResource()::getSlug();
 
         // Index Method
-        if($type === 'list' || $type === 'manager'){
+        if ($type === 'list' || $type === 'manager') {
             $routes[] = [
-              "table"=> $slug,
-              "method" => "get",
-              "slug" => $slug,
-              "callback" => function(Request $request) use ($query, $page, $table, $resource){
-                  return $this->index($request, $query, $page, $table, $resource);
-              },
-              "name" => $slug . ".index",
-              "middleware" => $middleware
+                "table" => $slug,
+                "method" => "get",
+                "slug" => $slug,
+                "callback" => function (Request $request) use ($query, $table, $resource) {
+                    return $this->index($request, $query, $table, $resource);
+                },
+                "name" => $slug . ".index",
+                "middleware" => $middleware
             ];
 
             // Delete Method
             $routes[] = [
-                "table"=> $slug,
+                "table" => $slug,
                 "method" => "delete",
                 "slug" => $slug. '/{model}',
-                "callback" => function ($record, Request $request) use ($page){
+                "callback" => function ($record, Request $request) use ($page) {
                     return $this->destroy($record, $request, $page);
                 },
                 "name" => $slug . ".destroy",
@@ -88,12 +75,12 @@ class FilamentAPIServices
         }
 
         // Store Method
-        if($type === 'create'  || $type === 'manager'){
+        if ($type === 'create'  || $type === 'manager') {
             $routes[] = [
-                "table"=> $slug,
+                "table" => $slug,
                 "method" => "post",
                 "slug" => $slug,
-                "callback" => function (Request $request) use ($page, $form, $resource){
+                "callback" => function (Request $request) use ($page, $form, $resource) {
                     return $this->store($request, $page, $form, $resource);
                 },
                 "name" => $slug . ".store",
@@ -102,12 +89,12 @@ class FilamentAPIServices
         }
 
         // Update Method
-        if($type === 'edit'  || $type === 'manager'){
+        if ($type === 'edit'  || $type === 'manager') {
             $routes[] = [
-                "table"=> $slug,
+                "table" => $slug,
                 "method" => "put",
                 "slug" => $slug. '/{model}',
-                "callback" => function ($record, Request $request) use ($page, $form, $resource){
+                "callback" => function ($record, Request $request) use ($page, $form, $resource) {
                     return $this->update($record, $request, $page, $form, $resource);
                 },
                 "name" => $slug . ".update",
@@ -116,12 +103,12 @@ class FilamentAPIServices
         }
 
         // View Method
-        if($type === 'view'  || $type === 'manager'){
+        if ($type === 'view'  || $type === 'manager') {
             $routes[] = [
-                "table"=> $slug,
+                "table" => $slug,
                 "method" => "get",
                 "slug" => $slug. '/{model}',
-                "callback" => function ($record, Request $request) use ($page, $resource){
+                "callback" => function ($record, Request $request) use ($page, $resource) {
                     return $this->show($record, $request, $page, $resource);
                 },
                 "name" => $slug . ".show",
@@ -136,7 +123,7 @@ class FilamentAPIServices
 
     public function routes(array $routes): void
     {
-        $this->routes = array_merge($this->routes , $routes);
+        $this->routes = array_merge($this->routes, $routes);
     }
 
     public function getRoutes(): array
@@ -144,58 +131,108 @@ class FilamentAPIServices
         return $this->routes;
     }
 
-    protected function index(Request $request,Builder $query,Page $page,Table $table,?string $resource=null)
+    protected function index(Request $request, Model $query, Table $table, ?string $resource = null)
     {
-        if($request->has('search') && !empty($request->get('search'))){
+        $primaryKey = $query->getKeyName();
+
+        if ($request->has('search') && !empty($request->get('search'))) {
             $searchableColumns = [];
-            foreach ($table->getColumns() as $column){
-                if($column->isSearchable()){
+            foreach ($table->getColumns() as $column) {
+
+                if ($column->isSearchable()) {
                     $searchableColumns[] = $column->getName();
                 }
             }
 
-            $query->where(function($query) use ($searchableColumns, $request){
-                foreach ($searchableColumns as $column){
-                    $query->orWhere($column, 'like', '%' . $request->get('search') . '%');
-                }
-            });
+            $query = $this->addWheres($query, $searchableColumns, $request);
         }
 
-        $visiableColumns = [];
-        foreach ($table->getColumns() as $column){
-            if($column->isVisible()){
-                $visiableColumns[] = $column->getName();
-            }
+        $visibleColumns = [];
+        [$visibleColumns, $relatedColumns] = $this->getParsedColumns($table->getColumns());
+        $visibleColumns[] = $primaryKey;
+
+        [$query, $visibleColumns] = $this->addRelations($query, $visibleColumns, $relatedColumns);
+
+        $query = $query->select(array_unique($visibleColumns));
+        if ($table->getDefaultSortColumn() && $table->getDefaultSortDirection()) {
+            $query = $query->orderBy($table->getDefaultSortColumn(), $table->getDefaultSortDirection());
         }
 
-        $query->select($visiableColumns);
-
-        if($table->getDefaultSortColumn() && $table->getDefaultSortDirection()){
-            $query->orderBy($table->getDefaultSortColumn(), $table->getDefaultSortDirection());
-        }
-
-        if($resource){
+        if ($resource) {
             return APIResponse::success($resource::collection($query->paginate($table->getDefaultPaginationPageOption())));
         }
 
         return APIResponse::success($query->paginate($table->getDefaultPaginationPageOption()));
     }
 
-    protected function show(int $record, Request $request,Page $page,?string $resource=null)
+    protected function addRelations($query, $visibleColumns, $relatedColumns)
     {
-        $resourceClass = app($page::getResource());
-        $indexPage = app($resourceClass->getPages()['index']->getPage());
-        $table = $resourceClass->table(new Table($indexPage));
+        foreach ($relatedColumns as $key => $columns) {
+
+            $query = $query->with([$key => function ($query) use ($key, $columns) {
+                if (str_contains(strtolower(get_class($query)), 'has')) {
+                    $columns[] = $query->getForeignKeyName();
+                } else {
+                    $columns[] = $query->getOwnerKeyName();
+                }
+                $query->select(array_unique($columns));
+            }]);
+
+
+            if (str_contains(strtolower(get_class($query->getRelation($key))), 'has')) {
+                $visibleColumns[] = $query->getRelation($key)->getLocalKeyName();
+            } else {
+                $visibleColumns[] = $query->getRelation($key)->getForeignKeyName();
+            }
+        }
+
+        return [$query, $visibleColumns];
+    }
+    protected function addWheres($query, $searchableColumns, $request)
+    {
+        return $query::where(function ($query) use ($searchableColumns, $request) {
+            foreach ($searchableColumns as $column) {
+                if (str_contains($column, '.')) {
+                    $columnSplit = explode('.', $column);
+                    $query->orWhereHas($columnSplit[0], function ($query) use ($columnSplit, $request) {
+                        $query->where($columnSplit[1], 'like', '%' . $request->get('search') . '%');
+                    });
+                    continue;
+                }
+                $query->orWhere($column, 'like', '%' . $request->get('search') . '%');
+            }
+        });
+    }
+    protected function getParsedColumns($columns)
+    {
+        $visibleColumns = [];
+        $relatedColumns = [];
+        foreach ($columns as $column) {
+            if ($column->isVisible()) {
+                if (str_contains($column->getName(), '.')) {
+                    $columnSplit = explode('.', $column->getName());
+                    $relatedColumns[$columnSplit[0]][] = $columnSplit[1];
+                    continue;
+                }
+                $visibleColumns[] = $column->getName();
+            }
+        }
+
+        return [$visibleColumns, $relatedColumns];
+    }
+
+    protected function show(int $record, Request $request, Page $page, ?string $resource = null)
+    {
         $record = app($page::getResource())->getModel()::find($record);
 
-        if($resource) {
+        if ($resource) {
             return APIResponse::success($resource::make($record));
         }
 
         return APIResponse::success($record);
     }
 
-    protected function store(Request $request, $page, $form,?string $resource=null)
+    protected function store(Request $request, $page, $form, ?string $resource = null)
     {
         $rules = [];
         $components = $form->getComponents();
@@ -207,22 +244,22 @@ class FilamentAPIServices
 
         $record = app($page::getResource())->getModel()::create($request->all());
 
-        if($resource) {
+        if ($resource) {
             return APIResponse::success($resource::make($record));
         }
 
         return APIResponse::success($record);
     }
 
-    protected function update($record, Request $request, $page, $form,?string $resource=null)
+    protected function update($record, Request $request, $page, $form, ?string $resource = null)
     {
         $record = app($page::getResource())->getModel()::find($record);
         $rules = [];
         $components = $form->getComponents();
         foreach ($components as $component) {
             $validation = $component->getValidationRules();
-            foreach ($validation as $key => $value){
-                if($value instanceof Unique){
+            foreach ($validation as $key => $value) {
+                if ($value instanceof Unique) {
                     $validation[$key] = $value->ignore($record->id);
                 }
             }
@@ -233,7 +270,7 @@ class FilamentAPIServices
 
         $record->update($request->all());
 
-        if($resource) {
+        if ($resource) {
             return APIResponse::success($resource::make($record));
         }
 
